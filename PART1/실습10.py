fe847e6ea9e91cd5d02a76df36990016bff28fc8
Park Mael IL <bitcocom@empas.com>
2019-05-29 10:52:48 +0900

    ok

--- PART1/실습10.py
+++ PART1/실습10.py
@@ -1,11 +1,29 @@
-# 실습 10 : 키보드로부터 학생 수를 입력받아 학생수 만큼 성적을 입력받는다.
-# 입력받은 학생들의 성적을 60점 이상이면 합격, 그렇치 않으면 불합격을 출력하는 프로그램을 만싀오.
+# 딕셔녀리 자료형(dict) : { key : value } -> JSON
+st = {"STATION_CD":"1916","STATION_NM":"소요산","LINE_NUM":"1","FR_CODE":"100"}
+print(type(st))
+print(st)
 
-num = int(input("학생수:"))
-jumList = []
+# 주어진 JSON 데이터(문자열)를 파이썬의 사전(dict) 자료형으로 변환하여 역이름을 출력하시오.
+import json
+jsonData = '{"STATION_CD":"1916","STATION_NM":"소요산","LINE_NUM":"1","FR_CODE":"100"}'
+print(type(jsonData))
+dicData=json.loads(jsonData)
+print("{0}역".format(dicData['STATION_NM']))
 
-# 입력한 학생수 많큼 점수를 리스트(jumList)에 저장한다.
+# 실습 10 : 파일에 저장된 JSON데이터를 읽어서 역명만 별도의 리스트로 만들어 출력하시오.
+f = open('station.txt', 'r' , encoding='utf-8')
+jsonStr = f.read()
+print(jsonStr)
+js = json.loads(jsonStr)
+if js:
+      station = []
+      point = js["SearchSTNBySubwayLineService"]
+      # data = json.loads(point)
+      for element in point["row"]:
+      # 가져온 역명에 "역"을 붙여줌
+          station.append(element["STATION_NM"] + "역")
 
+print(station)
 
-
-# 입력받은 리스트에서 성적을 처리한다.
+# 활용 방법
+# http://openapi.seoul.go.kr:8088/7443496561626974353343667a6843/json/SearchSTNBySubwayLineService/1/100/1
