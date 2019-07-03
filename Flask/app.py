ae8c1c69cfd560354c4c518696866370c8daf9f1
Park Mael IL <bitcocom@empas.com>
2019-07-03 09:50:49 +0900

    test commit

--- Flask/app.py
+++ Flask/app.py
@@ -1,58 +1,23 @@
 from flask import Flask
 from flask import render_template
-from datetime import time
-import pandas as pd
-from pandas import Series, DataFrame
-
+import pygal
 app = Flask(__name__)
 
-@app.route("/simple_chart")
-def chart():
-    df=pd.read_csv('final1101.csv');
-    legend = '서울'
-    yyyy = ["2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"]
-    price = [2234, 2133, 3124, 2567, 3178, 1980, 2100, 3112]
-    return render_template('chart.html', values=price, labels=yyyy, legend=legend)
+@app.route('/pygalexample')
+def pygalexample():
+	try :
+		graph = pygal.Line()
+		graph.title = '% Change Coolness of programming languages over time.'
+		graph.x_labels = ['2011','2012','2013','2014','2015','2016']
+		graph.add('Python',  [15, 31, 89, 200, 356, 900])
+		graph.add('Java',    [15, 45, 76, 80,  91,  95])
+		graph.add('C++',     [5,  51, 54, 102, 150, 201])
+		graph.add('All others combined!',  [5, 15, 21, 55, 92, 105])
+		graph_data = graph.render_data_uri()
+		return render_template("graphing.html", graph_data = graph_data)
 
-
-@app.route("/line_chart")
-def line_chart():
-    legend = 'Temperatures'
-    temperatures = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2,
-                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7,
-                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
-    times = ['12:00PM', '12:10PM', '12:20PM', '12:30PM', '12:40PM', '12:50PM',
-             '1:00PM', '1:10PM', '1:20PM', '1:30PM', '1:40PM', '1:50PM',
-             '2:00PM', '2:10PM', '2:20PM', '2:30PM', '2:40PM', '2:50PM']
-    return render_template('line_chart.html', values=temperatures, labels=times, legend=legend)
-
-
-@app.route("/time_chart")
-def time_chart():
-    legend = 'Temperatures'
-    temperatures = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2,
-                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7,
-                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
-    times = [time(hour=11, minute=14, second=15),
-             time(hour=11, minute=14, second=30),
-             time(hour=11, minute=14, second=45),
-             time(hour=11, minute=15, second=00),
-             time(hour=11, minute=15, second=15),
-             time(hour=11, minute=15, second=30),
-             time(hour=11, minute=15, second=45),
-             time(hour=11, minute=16, second=00),
-             time(hour=11, minute=16, second=15),
-             time(hour=11, minute=16, second=30),
-             time(hour=11, minute=16, second=45),
-             time(hour=11, minute=17, second=00),
-             time(hour=11, minute=17, second=15),
-             time(hour=11, minute=17, second=30),
-             time(hour=11, minute=17, second=45),
-             time(hour=11, minute=18, second=00),
-             time(hour=11, minute=18, second=15),
-             time(hour=11, minute=18, second=30)]
-    return render_template('time_chart.html', values=temperatures, labels=times, legend=legend)
-
+	except	Exception as e:
+	   return str(e)
 
 if __name__ == "__main__":
     app.run(debug=True)
